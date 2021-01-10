a4bc6b4ecc181e9b76ef31f1b050b5364b6a85ea
Eric <emetcalf9@gmail.com>
2021-01-09 18:44:49 -0800

    updating secret key setup for production use

--- bghweb/__init__.py
+++ bghweb/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=os.getenv('SECRET_KEY', 'dev'),
         DATABASE=os.environ['DATABASE_URL'],
         #DATABASE="bgh",
     )
